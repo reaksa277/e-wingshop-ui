98f6a1438f8d8835b5aaf859e143d38cdb69615b
reaksa <chhimchanreaksa@gmail.com>
2026-04-02 00:55:29 +0700

    clean the unuse file

--- app/dashboard/layout copy.tsx
+++
@@ -1,7 +0,0 @@
-import { DashboardLayout } from '@/components/dashboard/DashboardLayout';
-
-export default function DashboardRootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <DashboardLayout>{children}</DashboardLayout>
-  );
-}
